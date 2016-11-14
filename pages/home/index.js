c34d062cc033a7796c533644ea8784d18bd0b8cf
Ken Li <iliawnek@gmail.com>
2016-11-14 20:19:06 +0000

    Add Google icon to sign in button

--- pages/home/index.js
+++ pages/home/index.js
@@ -96,6 +96,18 @@ class HomePage extends React.Component {
       signInButton: {
         backgroundColor: '#4285F4',
         marginTop: 64,
+        paddingLeft: 0,
+      },
+      googleContainer: {
+        border: '1px solid #4285F4',
+        boxSizing: 'border-box',
+        width: 60,
+        height: 60,
+        backgroundColor: 'white',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginRight: 20,
       },
     };
 
@@ -108,6 +120,15 @@ class HomePage extends React.Component {
             onClick={this.props.signIn}
             style={styles.signInButton}
           >
+            <div style={styles.googleContainer}>
+              <svg style={styles.google} width={24} height={24} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48">
+                <path
+                  fill="#4285F4"
+                  d="M45,20h-2.5h-8.2h-9.8v8.5h11.8c-1.1,5.4-5.7,8.5-11.8,8.5c-7.2,0-13-5.8-13-13s5.8-13,13-13c3.1,0,5.9,1.1,8.1,2.9L39,7.5
+          C35.1,4.1,30.1,2,24.5,2c-12.2,0-22,9.8-22,22s9.8,22,22,22c11,0,21-8,21-22C45.5,22.7,45.3,21.3,45,20z"
+                />
+              </svg>
+            </div>
             ENTER WITH GOOGLE
           </Button>
         </div>
